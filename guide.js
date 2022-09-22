6e694178db72c1e2b84a7407de63ef9be24dbece
sanchari2018 <42566752+sanchari2018@users.noreply.github.com>
2022-09-22 21:06:53 +0530

    Add files via upload

---
+++ guide.js
@@ -0,0 +1,21 @@
+someValues.forEach((element, index) => {
+    console.log(`Current index: ${index}`);
+    console.log(element);
+});
+
+//for each element under someValues object , arrow will give the output
+.head{
+    background-color: black;
+    height : 0.5 px;
+    width : 60px;
+}
+
+.snake{
+    background-color: purple;
+}
+
+.food{
+    background-color: brown;
+    height : 15px;
+    width: 15px;
+}
